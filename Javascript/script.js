bcc2509d911f7ab0b2aebf4dd4215fe553b65d26
Harshvardhan Singh <84657351+Harshsingh99@users.noreply.github.com>
2023-11-20 22:35:59 +0530

    Portfolio

---
+++ Javascript/script.js
@@ -0,0 +1,18 @@
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        navbar.classList.add("header-scrolled");
+    } else (
+        navbar.classList.remove("header-scrolled");
+    )
+}
+
+// navbar hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+    a.addEventListener('click', function () {
+        navCollapse.classList.remove("show");
+    })
+})
+
